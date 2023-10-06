b47e0097a0766ee1c04745a0779d22bf872b2d9a
FrighteningFunction <szokoly-angyal.armand@outlook.com>
2023-10-07 00:43:34 +0200

    GameMain added. Bone modified in a template-way: static methods to import images from given directories of bones. Correct plug-in to main is in progess.

--- src/Bone.java
+++ src/Bone.java
@@ -1,9 +1,15 @@
 import javax.swing.*;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
 import static java.lang.Math.floor;
 
 /**
  * it represents an individual bone
+ *
+ * static bone image arrays need to be loaded manually!
  */
 public class Bone {
     public enum BoneType {
@@ -19,6 +25,10 @@ public class Bone {
     static Icon[] BIRD_BONES;
     static Icon[] HUMAN_BONES;
 
+    /**
+     * Spawns a bone randomly: chooses its type, then randomly pics
+     * an icon from the corresponding static icon container
+     */
     Bone(){
         boneType = BoneType.values()[(int)(Math.random()*3)];
 
@@ -33,6 +43,28 @@ public class Bone {
                 boneImage = HUMAN_BONES[(int)(Math.random()*HUMAN_BONES.length)];
                 break;
         }
+    }
+
+    public static void loadBoneImages() {
+        ALIEN_BONES = loadImagesFromDirectory("path/to/alien/bones");
+        BIRD_BONES = loadImagesFromDirectory("path/to/bird/bones");
+        HUMAN_BONES = loadImagesFromDirectory("path/to/human/bones");
+    }
+
+    private static Icon[] loadImagesFromDirectory(String directoryPath) {
+        File directory = new File(directoryPath);
+        File[] files = directory.listFiles((dir, name) -> name.toLowerCase().endsWith(".png"));
+
+        // Check if no files are found or if the directory does not exist.
+        if (files == null || files.length == 0) {
+            throw new RuntimeException("Fatal error: No images found in directory: " + directoryPath);
+        }
+
+        List<Icon> icons = new ArrayList<>();
+        for (File file : files) {
+            icons.add(new ImageIcon(file.getAbsolutePath()));
+        }
+        return icons.toArray(new ImageIcon[0]);
     }
 
 }
---
+++ src/GameMain.java
@@ -0,0 +1,40 @@
+public class GameMain {
+
+    private static UIManager uiManager;
+
+    public static void main(String[] args) {
+        initializeGame();
+        startGame();
+
+        // For the sake of simplicity, there's no endGame() in this initial version.
+        // It could be triggered by a game timer expiration or a specific event.
+    }
+
+    public static void initializeGame() {
+        // Set the look and feel to the system default
+        try {
+            javax.swing.UIManager.setLookAndFeel(javax.swing.UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        // Initialize the game UI
+        uiManager = new UIManager();
+    }
+
+    public static void startGame() {
+        uiManager.startGame();
+    }
+
+    public static void endGame() {
+        uiManager.endGame();
+
+        // Example game over logic:
+        // Display the score and possibly provide options to restart or exit.
+        // For now, simply print the game over message.
+        System.out.println("Game Over!");
+        // You can extend this to show a dialog or update the UI with the game over message and score.
+    }
+}
+
+
